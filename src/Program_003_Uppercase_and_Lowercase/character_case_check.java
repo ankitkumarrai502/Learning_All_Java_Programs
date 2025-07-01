9ee86deecd0ac097c6491b4da4ba9fda3593a800
Ankit Rai <ankitkumarrai502@gmail.com>
2025-07-02 01:09:37 +0530

    Updated character_case_check.java with code to detect uppercase/lowercase

--- src/Program_003_Uppercase_and_Lowercase/character_case_check.java
+++ src/Program_003_Uppercase_and_Lowercase/character_case_check.java
@@ -1,4 +1,15 @@
 package Program_003_Uppercase_and_Lowercase;
 
 public class character_case_check {
+    public static void main(String[] args) {
+        char ch = 'b';
+
+        if (Character.isUpperCase(ch)) {
+            System.out.println("Character is in Uppercase: " + ch);
+        }
+            else if(Character.isLowerCase(ch)) {
+                System.out.println("Character is in Lowercase: " + ch);
+            }
+
+    }
 }
